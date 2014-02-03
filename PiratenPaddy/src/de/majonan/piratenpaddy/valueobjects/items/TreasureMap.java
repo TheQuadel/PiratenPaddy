bc2324b07cf13badcc76ffdae15b71472c6f539c
TheQuadel <thequadel@live.de>
2014-02-04 00:21:36 +0100

    Created TreasureMap

---
+++ PiratenPaddy/src/de/majonan/piratenpaddy/valueobjects/items/TreasureMap.java
@@ -0,0 +1,37 @@
+package de.majonan.piratenpaddy.valueobjects.items;
+
+import de.majonan.piratenpaddy.valueobjects.Item;
+
+public class TreasureMap extends Item {
+
+	@Override
+	public void pull() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void open() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void lookAt() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
