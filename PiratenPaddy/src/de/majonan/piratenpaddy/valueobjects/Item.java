18c324ad090857859569f8d0df9054e826e10a6e
JGerdes <jonas.gerdes@gmx.net>
2014-02-04 00:06:17 +0100

    Created Class Item

---
+++ PiratenPaddy/src/de/majonan/piratenpaddy/valueobjects/Item.java
@@ -0,0 +1,14 @@
+package de.majonan.piratenpaddy.valueobjects;
+
+import java.awt.Image;
+
+public abstract class Item extends Entity {
+
+	private Image imageCollected;
+	
+	public abstract void pull();
+	public abstract void push();
+	public abstract void open();
+	public abstract void close();
+
+}
